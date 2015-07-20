1fbe8b84631d460165b9f099fdc1f8f1b05ea015
Markus Forsberg <markus.forsberg@gu.se>
2015-07-20 16:42:32 +0200

    First version of the paradigm class.

--- src/paradigm.py
+++ src/paradigm.py
@@ -1,60 +1,127 @@
-def load_file(f):
-    pass
+from collections import defaultdict
 
-# aakkosteta,aakkostaa,mood=indicative:tense=present:aspect=passive:polarity=negative
-# wordform \t lemma \t pos \t msd \t ID
-
-# p = Paradigm([(msd,Form)])
-
-# p('schr','i','b')
-
-# p.instansiate('schr','i','b')
-
-# p['tense']['Sg'] <- throws Uninst
-
-class Form:
-
-    def __init__(self):
-        pass
-
-class Slot:
-
-    def __init__(self, type, insts):
-
-    def is_var(self):
-        pass
-        
-    def is_str(self):
-        pass
-
-[''][1]['a'][2]['']
-
-[('MSD':'form'), ... ]
-            
 class Paradigm:
+    """A class representing a paradigm.
 
-    def __init__(self, forms):
-      self.forms = forms
-      self.num_vars = forms[0].num_vars
-      pass
-
-    def __call__(self,*insts):
-        
-    def instansiate(self, *insts): # vars = [(v_id, str)]
-        pass
+    Args:
+       form_msd:list(tuple)
+            Ex: [('1+en',[('tense','pres')]), ...,
+       var_insts:list(tuple)
+            Ex: [[('1','dimm')],[('1','dank')], ...]
+    """
+    
+    def __init__(self, form_msds, var_insts):
+      self.slts = None
+      self.forms = []
+      for (f,msd) in form_msds:
+          self.forms.append(Form(f,msd))
+      self.var_insts = var_insts
 
     def slots(self):
-        pass
+        """Compute the content
+         of the slots.
+        """
+        if self.slts != None:
+            return self.slts
+        else:
+            str_slots = zip(*[f.strs() for f in self.forms])
+        vt = defaultdict(list)
+        for vs in self.var_insts:
+            for (v,s) in vs:
+                vt[v].append(s)
+        var_slots = vt.items()
+        self.slts = []
+        (s_index,v_index) = (0,0)
+        for i in range(len(str_slots) + len(var_slots)): # interleave strings and variables
+            if i % 2 == 0:
+                self.slts.append(Slot(str_slots[s_index],False))    
+                s_index += 1
+            else:
+                self.slts.append(Slot(var_slots[v_index][1]))    
+                v_index += 1
+        return self.slts
 
-    def forms(self):
-        pass
+    def __call__(self,*insts):
+        table = []
+        for f in self.forms:
+            table.append(f(*insts))
+        return table
 
     def __str__(self):
-        pass
+        return "#".join([str(f) for f in self.forms])
+
+
+                
+class Form:
+    """A class representing a paradigmatic wordform and, possibly, its
+    morphosyntactic description.
+
+    Args:
+       form:str
+            Ex: 1+a+2
+       msd:list(tuple)
+            Ex: [('num','sg'),('case':'nom') .. ]
+                [] no msd available
+                [(None,'SGNOM')] no msd type available
+    """
+    def __init__(self, form, msd=[]):
+        (self.form,self.msd) = (form.split('+'), msd)
+                 
+    def __call__(self,*insts):
+        """Instantiate the variables of the wordform.
+           Args:
+            insts: fun args
+                   Ex: f('schr','i','b') 
+        """
+        (w,vindex) = ('',0) 
+        for p in self.form:
+            if p.isdigit(): # is a variable
+                w += insts[vindex]
+                vindex += 1
+            else:
+                w += p
+        return (w, self.msd)
+
+    def strs(self):
+        """Collects the strings in a wordform.
+           A variable is assumed to be surrounded by (possibly empty) strings.
+        """
+        ss = []
+        if self.form[0].isdigit():
+           ss.append('')
+        for i in range(len(self.form)):
+            if not(self.form[i].isdigit()) or (len(self.form) < i and self.form[i+1].isdigit()):
+             ss.append(self.form[i])
+        if self.form[-1].isdigit():
+            ss.append('')
+        return ss
         
-    def __iter__(self):
-        pass
+class Slot:
+    """A class representing a slot in a wordform.
+
+       Args:
+        insts: list(str)
+          Ex: ['spr','st']
+        is_var: bool
+          Is it a variable slot or not?
+    """
+    
+    def __init__(self, insts, is_var = True):
+        self.is_var = is_var
+        self.insts = insts
         
-    def __getitem__(self):
-        pass
-           
+    def is_var(self):
+        return self.is_var
+        
+    def is_str(self):
+        return not(self.is_var)
+
+    def members(self):
+        return self.insts
+
+if __name__ == '__main__':
+    p = Paradigm([('1+i+2+er',[]), ('1+a+2',[])],[[('1','spr'),('2','ng')], [('1','st'),('2','ck')]])
+    # print the content of the slots
+    for (i,s) in enumerate(p.slots()):
+        print '%d: %s' % (i," ".join(s.members()))
+
