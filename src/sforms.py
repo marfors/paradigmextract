bce61334efaea1c29366f6b2d2be3e4ab3728b34
Markus Forsberg <markus.forsberg@gu.se>
2015-07-24 22:23:42 +0200

    More information added to sform output.

--- src/sforms.py
+++ src/sforms.py
@@ -12,21 +12,22 @@ def lalign(ss):
 
 ps = paradigm.load_file(sys.argv[1])
 
-pss = [(n,p.p_forms(), ' '.join(['[%s]' % ",".join(list(set(s.members()))[:5]) for s in p.slots() if s.is_var()])) for (_,n,p) in ps]
+pss = [(n,c, p.p_forms(), ' '.join(['[%s]' % ",".join(list(set(s.members()))[:5]) for s in p.slots() if s.is_var()])) for (c,n,p) in ps]
 result = defaultdict(set)
 
-for (n1,fs,s) in pss:
+for (n1,c,fs,s) in pss:
     for (f,wf) in fs:
-        result[f].add((wf, n1, s))
+        result[f].add((c,n1, wf, s))
 
 result = [(len(xs),f,xs) for (f,xs) in result.iteritems()]
 result.sort(reverse=True)
 
-for (c,f,xs) in result:
+for (n,f,xs) in result:
       if len(xs) > 1:
-        wfs = ralign([x[0] for x in xs])
-        lms = lalign(['p_' + x[1] for x in xs])
-        sls = [x[2] for x in xs]
+        xs = sorted(xs,reverse=True)
+        wfs = ralign([x[2] for x in xs])
+        lms = lalign(['p_%s %d' % (x[1],x[0]) for x in xs])
+        sls = [x[3] for x in xs]
         print ((len(wfs[0])+len(lms[0]) - len(f)-3)*' ' + '=> %s <=' % f).encode('utf-8')
         for t in zip(lms,wfs,sls):
             print ('%s%s  %s' % t).encode('utf-8')
