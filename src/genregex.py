3d564679cddadd0fb8b842d1469a9dc513be45fa
Mans Hulden <mans.hulden@gmail.com>
2015-07-27 18:36:25 +0200

    Cleanup, documentation

--- src/genregex.py
+++ src/genregex.py
@@ -4,7 +4,42 @@ class genregex:
        The main strategy is to find those complete strings, suffixes, or
        prefixes in the set that seem to be restricted in their distribution
        and issue a regular expression (python or foma), that matches a limited
-       set of strings."""
+       set of strings.
+
+       This is achieved through a number of tests.
+       We first make the assumption that strings in a set are drawn from a
+       uniform distribution with n members.  Then, we
+       (1) ask how likely it is to draw this particular sequence, assuming the
+       set really has n+1 members (i.e. where we never saw the n+1th member)
+       which is p = 1-(1/(n+1)) ** num_draws
+       where num draws is the length of the list of strings. If this p < 0.05 (by default)
+       we assume we have seen all members of the set and declare the set to be fixed.
+       
+       If the set of members is not found to be fixed, we further investigate
+       the suffixes and prefixes in the set. We the find the longest
+        (2a) suffix that can be assumed to be drawn from a fixed set
+        (2b) prefix that fulfills the same conditions.
+       We also examine the distribution of string lengths. If, by the same analysis,
+       the lengths of strings can be assumed to be drawn from a fixed set, we
+       limit the set of allowable lengths.
+
+       A regex can be returned either for python or foma. The regex
+       may need to check both the prefix and suffixes separately, which
+       is easily done in a foma-style regex since we can intersect the
+       prefix and suffix languages separately:
+       [?* suffixes] & [prefixes ?*]
+       However, this can't be directly done in Python.  To simulate this,
+       we check the suffix (and possible length constraints) by a lookahead
+       which doesn't consume any symbols, before the checking the prefix.
+
+       Example:
+       >>>words = ['ab','ab','ab','ba','ba','ba','ab','ba','a','b']
+       >>>r = genregex.genregex(words)
+       >>>print r.pyregex()
+       ^(?=.*(a|b)$)(?=.{1,2}$)(a|b)
+       >>>print r.fomaregex()
+       [?* [{a}|{b}]] & [?^{1,2}] & [[{a}|{b}] ?*]
+       """
     
     def __init__(self, strings, pvalue = 0.05):
         self.strings = strings
@@ -17,21 +52,19 @@ class genregex:
         self.prefixset = set()
         self.suffixset = set()
         self.lenrange = ()
-        # Case (1): if the totality of strings seem to have a limited distribution
+        # Case (1): if the totality of strings seems to have a limited distribution
         if self._significancetest(self.numstrings, len(set(self.strings))):
             self.stringset = set(self.strings)
             return
         # Case (2a): find longest suffix that has limited distribution
         for i in xrange(-self.minlen, 0):
             suffstrings = map(lambda x: x[i:], self.strings)
-            print "SUFFTEST:", suffstrings
             if self._significancetest(len(suffstrings), len(set(suffstrings))):
                 self.suffixset = set(suffstrings)
                 break
         # Case (2b): find longest prefix that has limited distribution
         for i in xrange(self.minlen, 0, -1):
             prefstrings = map(lambda x: x[:i], self.strings)
-            print "PREFTEST:", prefstrings
             if self._significancetest(len(prefstrings), len(set(prefstrings))):
                 self.prefixset = set(prefstrings)
                 break
