30d5b56e8633b44449e72924885959548b642965
Mans Hulden <mans.hulden@gmail.com>
2015-09-21 11:38:58 -0600

    Add options -n -s

--- src/morphanalyzer.py
+++ src/morphanalyzer.py
@@ -4,7 +4,10 @@
 # -o  recreate original data (all vars must be exactly instantiated as seen in training data)
 # -c  constrain variables by generalizing (default pvalue = 0.05)
 # -u  unconstrained (all variables are defined as ?+)
-# -p <pvalue>  use <pvalue> together with -c
+# -p  <pvalue>  use <pvalue> together with -c
+# -s  keep different analyzers separate instead of merging with priority union
+#     (may be necessary for some analyzers)
+# -n  name of binary foma file to compile to
 
 # Any combination of the above may be used. The analyzers are combined by
 # priority union, e.g. -o -c -u would yield an analyzer
@@ -75,11 +78,11 @@ def paradigms_to_foma(paradigms, grammarname, pval):
 def main(argv):
 
     options, remainder = getopt.gnu_getopt(argv[1:],
-        'ocup:', ['original','constrained','unconstrained','pvalue'])
+        'ocup:sn:', ['original','constrained','unconstrained','pvalue','separate','name'])
 
     pv = 0.05
 
-    (Goriginal, Gconstrained, Gunconstrained) = False, False, False
+    (Goriginal, Gconstrained, Gunconstrained, Gseparate, Gname) = False, False, False, False, 'analyzer.bin'
     for opt, arg in options:
         if opt in ('-o', '--original'):
             Goriginal = True
@@ -87,8 +90,12 @@ def main(argv):
             Gconstrained = True
         elif opt in ('-u', '--unconstrained'):
             Gunconstrained = True
+        elif opt in ('-s', '--separate'):
+            Gseparate = True
         elif opt in ('-p', '--pvalue'):
             pv = float(arg)
+        elif opt in ('-n', '--name'):
+            Gname = arg
 
     paradigms = paradigm.load_file(remainder[0])
 
@@ -103,7 +110,13 @@ def main(argv):
         print a.encode('utf-8')
 
     if len(analyzers) > 0:
-        print 'regex ' + u' .P. '.join(analyzernames) + ';'
+        if Gseparate == True:
+            for a in analyzernames:
+                print 'regex ' + a + ';'
+        else:
+            print 'regex ' + u' .P. '.join(analyzernames) + ';'
+        print 'save stack ' + Gname
+
 
 if __name__ == "__main__":
     main(sys.argv)
